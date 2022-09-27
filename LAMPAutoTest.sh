1b7d74cc35a0461bf1bd9fad3e21d394b0367147
bbnagitesting <114498860+bbnagitesting@users.noreply.github.com>
2022-09-27 09:47:08 -0500

    Update LAMPAutoTest.sh

--- LAMPAutoTest.sh
+++ LAMPAutoTest.sh
@@ -1,24 +1,37 @@
-# LAMP install script (CentOS7 /tmp/LAMP_up.sh)
+# LAMP install script
+
+echo Enter your computer's IP address
+read myip
 
 sudo yum install httpd
+$ yes | ./LAMPAutoTest.sh
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
 
 sudo yum install mariadb-server
+$ yes | ./LAMPAutoTest.sh
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 
 sudo mysql_secure_installation
+# at this point it will ask for root password, it will be "" 
+# set a password
+# Y
+# Disallow root login remotely? n  # this is important unless you want to have to make a new user or have to login with your linux client
+# Y
+# Y
 
 sudo yum install php php-mysql
+$ yes | ./LAMPAutoTest.sh
+$ yes | ./LAMPAutoTest.sh
 sudo systemctl restart httpd.service
 "<?php ?>" >> /var/www/html/info.php
 
-sudo yum install epel-release
 sudo yum install phpmyadmin
+$ yes | ./LAMPAutoTest.sh
+$ yes | ./LAMPAutoTest.sh
 
-echo Enter your computer's IP address
-read myip
+
 replace '127.0.0.1' $myip -- /etc/httpd/conf.d/phpMyAdmin.conf
 
 sudo systemctl restart httpd
