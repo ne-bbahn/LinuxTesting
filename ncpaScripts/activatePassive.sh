7ccc8d3fbba23cc7118d7d03ca001a494846cb6c
ne-bbahn <bbahnernagios@gmail.com>
2024-03-04 15:07:13 -0600

    adds script to open firewall 5693/tcp for NCPA and activates passive

---
+++ ncpaScripts/activatePassive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HOST=$1
+USER="root"
+PORT="22"
+
+ssh -p "$PORT" "$USER@$HOST" << EOF
+
+firewall-cmd --add-port=5693/tcp --permanent
+firewall-cmd --reload
+sed -i 's/handlers = None/handlers = nrdp/' /usr/local/ncpa/etc/ncpa.cfg
+sed -i '5,\$s/^#//' /usr/local/ncpa/etc/ncpa.cfg.d/example.cfg
+sudo systemctl restart ncpa
+
+EOF
