eebfe3dcc540d39a11beef3c5c942035ca52bd17
ne-bbahn <bbahnernagios@gmail.com>
2024-03-04 16:19:04 -0600

    updating to work on Cent & Ubuntu

--- ncpaScripts/activatePassive.sh
+++ ncpaScripts/activatePassive.sh
@@ -6,8 +6,15 @@ PORT="22"
 
 ssh -p "$PORT" "$USER@$HOST" << EOF
 
-firewall-cmd --add-port=5693/tcp --permanent
-firewall-cmd --reload
+OS=$(grep -Eo '(Ubuntu|CentOS)' /etc/*release)
+
+if [[ "$OS" == "Ubuntu" ]]; then
+    sudo ufw allow 5693/tcp
+elif [[ "$OS" == "CentOS" ]]; then
+    sudo firewall-cmd --add-port=5693/tcp --permanent
+    sudo firewall-cmd --reload
+fi
+
 sed -i 's/handlers = None/handlers = nrdp/' /usr/local/ncpa/etc/ncpa.cfg
 sed -i '5,\$s/^#//' /usr/local/ncpa/etc/ncpa.cfg.d/example.cfg
 sudo systemctl restart ncpa
