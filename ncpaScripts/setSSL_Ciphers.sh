582eb339a09f3143bd96441c4970d93c3986f479
ne-bbahn <bbahnernagios@gmail.com>
2024-03-04 14:41:52 -0600

    moved files in a folder

---
+++ ncpaScripts/setSSL_Ciphers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+HOST=$1
+USER="root"
+PASS="welcome"
+PORT="22"
+
+ssh -p "$PORT" "$USER@$HOST" << EOF
+
+sed -i 's/# ssl_ciphers = <list of ciphers>/ssl_ciphers = AES256-GCM-SHA384:AES256-SHA256:AES256-SHA:CAMELLIA256-SHA/' /usr/local/ncpa/etc/ncpa.cfg
+
+EOF
