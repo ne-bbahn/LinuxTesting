a61471fa86eee153007477c99892a8a92d64afd5
ne-bbahn <114498860+ne-bbahn@users.noreply.github.com>
2024-03-06 15:28:49 -0600

    Update setSSL_Ciphers.sh

--- ncpaScripts/setSSL_Ciphers.sh
+++ ncpaScripts/setSSL_Ciphers.sh
@@ -8,5 +8,6 @@ PORT="22"
 ssh -p "$PORT" "$USER@$HOST" << EOF
 
 sed -i 's/# ssl_ciphers = <list of ciphers>/ssl_ciphers = AES256-GCM-SHA384:AES256-SHA256:AES256-SHA:CAMELLIA256-SHA/' /usr/local/ncpa/etc/ncpa.cfg
+systemctl restart ncpa
 
 EOF
